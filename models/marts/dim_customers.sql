38ea49fb61b11a54211cda4c6fa29c9c33b8cd2b
pauloestevaotaveira@outlook.com <pauloestevaotaveira@outlook.com>
2022-05-31 23:16:59 +0000

    commit

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -23,7 +23,7 @@ with
      select *
      from {{ref('stg_businessentity')}}
 )
-, address as(
+, personaddress as(
      select *
      from {{ref('stg_address')}}
 )
@@ -35,15 +35,30 @@ with
      select *
      from {{ref('stg_stateprovince')}}
 )
+, salesperson as(
+     select *
+     from {{ref('stg_salesperson')}}
+)
 , complete as (
     select 
         customers.customerid
         , person.businessentityid
-        , concat(person.firstname,' ',person.lastname)
+        , concat(person.firstname,' ',person.lastname) as customername
+        , creditcard.cardtype
+        , territory.territoryid
+        , personaddress.city
+        , stateprovince.name as state
+        , territory.name as country
 
     from customers
     left join person on customers.customerid = person.businessentityid
-    
+    left join personcreditcard on person.businessentityid = personcreditcard.businessentityid
+    left join creditcard on personcreditcard.creditcardid = creditcard.creditcardid
+    left join salesperson on person.businessentityid = salesperson.businessentityid
+    left join territory on salesperson.territoryid = territory.territoryid
+    left join businessentityadress on person.businessentityid = businessentityadress.businessentityid
+    left join personaddress on businessentityadress.addressid = personaddress.addressid
+    left join stateprovince on personaddress.stateprovinceid = stateprovince.stateprovinceid
 )
 
 select * from complete
