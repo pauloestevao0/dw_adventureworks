89fc7eaa265691bbf87750e257f5fc8120ab9d0e
pauloestevaotaveira@outlook.com <pauloestevaotaveira@outlook.com>
2022-06-01 00:56:45 +0000

    commit

--- models/marts/fact_orders.sql
+++ models/marts/fact_orders.sql
@@ -15,19 +15,27 @@ with
      select *
      from {{ref('stg_customer')}}
 )
+, salesreason as(
+     select *
+     from {{ref('stg_salesreason')}}
+)
 
 , complete as (
     select 
         orders.salesorderid
+        , orders.customerid
         , orders.orderdate
         , details.orderqty
         , details.salesorderdetailid
         , details.unitprice
         , details.productid
         , reason.salesreasonid
+        , salesreason.name as reasonname
+
     from orders
     left join details on orders.salesorderid = details.salesorderid
     left join reason on orders.salesorderid = reason.salesorderid
+    left join salesreason on reason.salesreasonid = salesreason.salesreasonid
 )
 
 select * from complete
